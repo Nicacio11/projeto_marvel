937ec724bc7699a8110c4d4ad481510ae7767b1d
Vitor Nicacio <vitornicacio10@hotmail.com>
2021-08-02 18:53:04 -0300

    fix: add character import to app module

--- marvel-api/src/app.module.ts
+++ marvel-api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { MarvelApiService } from './marvel-api/marvel-api.service';
 import { MarvelApiModule } from './marvel-api/marvel-api.module';
+import { CharacterModule } from './character/character.module';
 import DbModule from './config/db.module';
 import EnvService from './config/env.service';
 import UsuarioModule from './usuario/usuario.module';
@@ -14,6 +15,7 @@ import UsuarioModule from './usuario/usuario.module';
     UsuarioModule,
     AuthModule,
     MarvelApiModule,
+    CharacterModule,
   ],
   providers: [ConfigService, EnvService],
 })
