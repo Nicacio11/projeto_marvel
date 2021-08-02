c9a6cc37a4b398a509f77b8330423a597b229480
Vitor Nicacio <vitornicacio10@hotmail.com>
2021-08-02 09:09:21 -0300

    feat(add api to workspace): :construction: add api to workspace

---
+++ marvel-api/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['**/*.spec.ts', 'node_modules/', 'test/', '.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'off',
+    'no-useless-constructor': 'off',
+    'import/no-extraneous-dependencies': ['off'],
+    '@typescript-eslint/indent': [2, 2],
+    camelcase: 'off',
+    '@typescript-eslint/camelcase': ['off'],
+    'class-methods-use-this': ['off'],
+    'no-plusplus': ['off'],
+    'linebreak-style': ['error', 'windows'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'import/prefer-default-export': 'off',
+    'lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+  },
+};
