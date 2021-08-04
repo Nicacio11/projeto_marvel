6b9f672d4a46d51ec63cc4325e5e7467439658d6
Vitor Nicacio <vitornicacio10@hotmail.com>
2021-08-03 22:14:43 -0300

    feat: :sparkles: include login and register layout

--- marvel-app/src/app/pages/auth/login/login.component.ts
+++ marvel-app/src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  formGroup: FormGroup = this.fb.group({});
+  validationsMessage = {
+    email: [
+      { type: 'required', message: 'Campo obrigatório.' },
+      { type: 'email', message: 'Email invalido' },
+    ],
+    senha: [
+      { type: 'required', message: 'Campo obrigatório.' },
+      {
+        type: 'minlength',
+        message: 'É necessário que a senha possua no mínimo 6 caractéres.',
+      },
+    ],
+  };
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    })
   }
+  logar(jsonValue: any) {
 
+  }
 }
--- marvel-app/src/app/pages/auth/register/register.component.ts
+++ marvel-app/src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  formGroup: FormGroup = this.fb.group({});
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      passwords: this.fb.group({
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+        confirm_senha: ['', Validators.required]
+      }, { validator: this.compararSenha })
+    })
+  }
+  compararSenha(fb: FormGroup) {
+    const confirmSenhaCtrl = fb.get('confirm_senha');
+    if (confirmSenhaCtrl?.errors == null || 'mismatch' in confirmSenhaCtrl?.errors) {
+      if (confirmSenhaCtrl?.value !== fb.get('senha')?.value) {
+        confirmSenhaCtrl?.setErrors({ mismatch: true });
+      } else {
+        if (confirmSenhaCtrl?.hasError('mismatch')) { delete confirmSenhaCtrl.errors!.mismatch; }
+      }
+    }
   }
 
 }
