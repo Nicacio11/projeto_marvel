4835c8cc8064a5e3ae279c8234d8b5c20d309579
Vitor Nicacio <vitornicacio10@hotmail.com>
2021-08-04 20:15:39 -0300

    feat: :sparkles: add my favorite comic

---
+++ marvel-app/src/app/pages/internal/comic/comic-favorite/comic-favorite.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { UsuarioDTO } from 'src/app/core/models/usuario.dto';
+import { PerfilService } from '../../perfil/perfil.service';
+import { ComicService } from '../comic.service';
+import { ComicFavorite } from './comic.model';
+
+@Component({
+  selector: 'app-comic-favorite',
+  templateUrl: './comic-favorite.component.html',
+  styleUrls: ['./comic-favorite.component.css']
+})
+export class ComicFavoriteComponent implements OnInit {
+
+  comics: any;
+  like = 1;
+  usuarioDto: UsuarioDTO;
+
+  constructor(private comicService: ComicService, private perfilService: PerfilService, private titleService: Title) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Comics favoritas')
+    this.perfilService.get().subscribe(x => {
+      this.usuarioDto = x
+      this.comicService.getByUserId(x.id!).subscribe(y => console.log(this.comics = y))
+    });
+
+  }
+
+  remove(objComic: any) {
+    console.log(objComic)
+    this.comicService.delete(objComic.id).subscribe(() => {
+      this.comics = this.comics.filter((y: ComicFavorite) => y.id !== objComic.id)!
+    })
+  }
+}
---
+++ marvel-app/src/app/pages/internal/comic/comic-favorite/comic.model.ts
@@ -0,0 +1,12 @@
+export interface ComicFavorite {
+  id: number;
+  id_usuario: string;
+  id_comic: number;
+  title: string;
+  description: string;
+  thumbnailPath: string;
+  thumbnailPathExtension: string;
+  pageCount: string;
+  date: Date;
+
+}
