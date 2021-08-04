4e96ceed421eaf5b7c996ef3d9e358ec4f032b89
Vitor Nicacio <vitornicacio10@hotmail.com>
2021-08-04 13:27:45 -0300

    refactor: :recycle: remove perfil component from auth module

--- marvel-app/src/app/pages/auth/auth.module.ts
+++ marvel-app/src/app/pages/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { PerfilComponent } from './perfil/perfil.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -49,7 +48,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   declarations: [
     LoginComponent,
     RegisterComponent,
-    PerfilComponent
   ],
   imports: [
     AuthRoutingModule,
